4d46fc29a86cf7eeaca840ff9b86277134d741c6
Daniel <limadanielpereira@gmail.com>
2024-10-03 17:48:08 -0300

    Remove CSS do HTML

--- php/index.php
+++ php/index.php
@@ -3,16 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>Dr. Saúde</title>
-    <!-- Meta tags necessárias -->
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- CSS do Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Ícones do Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="shortcut icon" href="../imgs/hospital(1).svg" type="image/x-icon">
 </head>
 <body>
-    <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Dr. Saúde</a>
@@ -39,14 +35,14 @@
         </div>
     </nav>
 
-    <!-- Conteúdo Principal -->
+
     <div class="container text-center my-5">
         <h1>O consultório médico número 1 da América Latina!</h1>
         <p class="lead">Sem se preocupar com deslocamento! Simples, rápido e fácil.</p>
         <p class="lead">Marque consultas sem ligar e sem sair de casa!</p>
     </div>
 
-    <!-- Imagens -->
+
     <div class="container mb-5">
         <div class="row">
             <div class="col-md-6">
@@ -58,12 +54,12 @@
         </div>
     </div>
 
-    <!-- Rodapé -->
+
     <footer class="bg-light text-center py-3">
         <p>&copy; Todos os direitos são reservados ao Dr. Saúde</p>
     </footer>
 
-    <!-- Scripts do Bootstrap -->
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
