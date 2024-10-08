740cd59839e3c4475d4f9488d5cec356f2f576f2
Daniel <limadanielpereira@gmail.com>
2024-10-08 13:45:34 -0300

    Retifica código

--- js/input_para_consulta.js
+++ js/input_para_consulta.js
@@ -8,19 +8,19 @@ document.addEventListener('DOMContentLoaded', function () {
         var value = input.value;
         var length = value.length;
 
-        // Impede que caracteres não numéricos sejam digitados
+        
         if (isNaN(value[length - 1])) {
             input.value = value.substring(0, length - 1);
             return;
         }
 
-        // Adiciona as barras nas posições corretas (formato DD/MM/AAAA)
+        
         if (length === 2 || length === 5) {
             input.value += '/';
         }
     });
     
-    // Valida a data no evento de 'blur' (quando o campo perde o foco)
+    
     document.getElementById('data').addEventListener('blur', function () {
         var input = this.value;
         var dateParts = input.split('/');
@@ -32,22 +32,21 @@ document.addEventListener('DOMContentLoaded', function () {
             var today = new Date();
             var selectedDate = new Date(year, month - 1, day);
 
-            // Verifica se a data é válida e se é posterior ao dia de hoje
+            
             if (selectedDate < today) {
-                // Atualiza o alerta para mostrar a mensagem de erro
+                
                 alertHeading.textContent = 'Data Inválida';
                 alertMessage.textContent = 'A data deve ser posterior ao dia atual.';
-                alertBox.classList.remove('d-none'); // Torna o alerta visível
-                this.value = '';  // Limpa o campo
+                alertBox.classList.remove('d-none'); 
+                this.value = '';  
             } else {
-                alertBox.classList.add('d-none'); // Oculta o alerta se a data for válida
+                alertBox.classList.add('d-none'); 
             }
         } else {
-            // Atualiza o alerta para mostrar a mensagem de erro
             alertHeading.textContent = 'Formato Inválido';
             alertMessage.textContent = 'O formato da data deve ser DD/MM/AAAA.';
-            alertBox.classList.remove('d-none'); // Torna o alerta visível
-            this.value = '';  // Limpa o campo
+            alertBox.classList.remove('d-none');
+            this.value = '';  
         }
     });
 });
